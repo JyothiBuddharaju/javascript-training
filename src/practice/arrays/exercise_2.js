b2cdfd88680e84339c781bf5f1f8a91222e7f7cf
JyothiBuddharaju <jyothi.addala@bbc.co.uk>
2019-09-30 17:09:19 +0100

    Arrays -practice-excercise2

--- src/practice/arrays/exercise_2.js
+++ src/practice/arrays/exercise_2.js
@@ -6,6 +6,16 @@
  * NOTE: Use the forEach method
  */
 
-const vowelsCounter = () => {}
+const vowelsCounter = (inputString) => {
+  const newArray = inputString.split();
+  const vowelsArray = ['a', 'e', 'i', 'o', 'u'];
+  let counter = 0;
+  newArray.forEach((element, counter) => {
+    if (vowelsArray.includes(element)) {
+      counter = counter + 1;
+    }
+  });
+  return counter;
+}
 
 export default vowelsCounter
